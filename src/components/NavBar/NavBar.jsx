0ff9474778884489b24ee21c8f6594e8f6a2714f
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-02-17 21:47:45 -0500

    Create a component to show logo and navigation icons

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../../Assets/images/logo.svg';
+
+import './NavBar.scss';
+
+const NavBar = () => {
+  return (
+    <>
+      <div className='NavBar'>
+        <div>
+          <img src={logo} alt='logo' />
+        </div>
+        <div>
+          <button className='Buttons'>Home</button>
+          <button className='Buttons'>New</button>
+          <button className='Buttons'>Popular</button>
+          <button className='Buttons'>Trending</button>
+          <button className='Buttons'>Categories</button>
+        </div>
+      </div>
+    </>
+
+  );
+};
+
+export default NavBar;
