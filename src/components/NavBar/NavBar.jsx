e31e7c483cc60a6b1aedb1956403206e27a44fec
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-02-22 15:22:53 -0500

    Add images for smaller screens

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+
 import logo from '../../Assets/images/logo.svg';
+import menuImage from '../../Assets/images/icon-menu.svg';
+import closeMenuImage from '../../Assets/images/icon-menu-close.svg';
+
 
 import './NavBar.scss';
 
 const NavBar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <>
       <div className='NavBar'>
@@ -11,11 +18,41 @@ const NavBar = () => {
           <img src={logo} alt='logo' className='logo' />
         </div>
         <div>
-          <button className='Buttons'>Home</button>
-          <button className='Buttons'>New</button>
-          <button className='Buttons'>Popular</button>
-          <button className='Buttons'>Trending</button>
-          <button className='Buttons'>Categories</button>
+
+          <div className='nav-names'>
+            <button className='buttons'>Home</button>
+            <button className='buttons'>New</button>
+            <button className='buttons'>Popular</button>
+            <button className='buttons'>Trending</button>
+            <button className='last-buttons'>Categories</button>
+          </div>
+
+          <div>
+            <img className='menu-icon' src={menuImage} alt='menu' onClick={() => { setToggle(true); }} />
+            {toggle &&
+              <motion.div
+                className='side-menu'
+                whileInView={{ x: [300, 0] }} /* 300px */
+                transition={{ duration: 1, ease: 'easeOut' }}
+              >
+
+                <div>
+                  <ul style={{ marginTop: "6em", paddingLeft: '0' }}>
+                    <button className='side-menu-buttons'>Home</button>
+                    <button className='side-menu-buttons'>New</button>
+                    <button className='side-menu-buttons'>Popular</button>
+                    <button className='side-menu-buttons'>Trending</button>
+                    <button className='side-menu-buttons'>Categories</button>
+                  </ul>
+                </div>
+
+                <div>
+                  <img className='menu-icon' src={closeMenuImage} alt='close menu' onClick={() => { setToggle(false); }} />
+                </div>
+
+              </motion.div>}
+          </div>
+
         </div>
       </div>
     </>
